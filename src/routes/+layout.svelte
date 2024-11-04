d4aea10397310f69e872aab5a9bd6fe0b0e262df
rovic-john-p-de-guia <rovicjohn.p.deguia.08@gmail.com>
2024-11-04 09:50:59 +0800

    fix routing error

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,7 @@
 </script>
 
 <div class="body">
+    <!-- Navbar -->
     <div class="navbar">
         <button class="icon-button" aria-label="Menu button">
             <svg class="w-6 h-6 text-gray-800 dark:text-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" aria-hidden="true">
@@ -13,6 +14,13 @@
         <h1>Emergency Response</h1>
     </div>
 
+    <!-- Main Content -->
+    <main>
+        <!-- Add your page-specific content here -->
+        <slot />
+    </main>
+
+    <!-- Footer -->
     <footer class="footer">
         <a class="links" href="/project2/home">
             <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
@@ -34,9 +42,8 @@
         </a>
         <a class="links" href="/project2/hotlines">
             <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-				<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 9H5a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h6m0-6v6m0-6 5.419-3.87A1 1 0 0 1 18 5.942v12.114a1 1 0 0 1-1.581.814L11 15m7 0a3 3 0 0 0 0-6M6 15h3v5H6v-5Z"/>
-			  </svg>
-			  
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 9H5a1 1 0 0 0-1 1v4a1 1 0 0 0 1 1h6m0-6v6m0-6 5.419-3.87A1 1 0 0 1 18 5.942v12.114a1 1 0 0 1-1.581.814L11 15m7 0a3 3 0 0 0 0-6M6 15h3v5H6v-5Z"/>
+            </svg>
             Hotlines
         </a>
     </footer>
