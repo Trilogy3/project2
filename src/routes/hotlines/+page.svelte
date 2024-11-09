fe4f97be6de93eba60d0ba71f59e647e1138e3ff
damdaph <davejustinarcilla@gmail.com>
2024-11-09 08:36:35 +0800

    undo commit

--- src/routes/hotlines/+page.svelte
+++ src/routes/hotlines/+page.svelte
@@ -1,5 +1,6 @@
 <script>
-    console.log("Hotlines route accessed");
-  </script>
-  
-  <h1>Hotlines</h1>
+    // Any specific logic for the status page can go here
+</script>
+
+<h1>Status Page</h1>
+<p>This page provides the current status of various functionalities.</p>
